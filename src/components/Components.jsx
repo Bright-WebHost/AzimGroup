e1ad23fe3c857f018074c7f18c55af6efd1b7013
Mohammed Junaid <mohadjunaid212@gmail.com>
2025-12-09 14:14:55 +0530

    code commit 1

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -66,7 +66,7 @@ import Careers from './Pages/Careers';
 class Components extends React.Component {
     render() {
         return (
-            <BrowserRouter basename="/AzimGroup/">
+            <BrowserRouter basename="/AzimFacilities/">
                 <ScrollToTop />
                 <div className="page-wraper">
                         <Routes>
